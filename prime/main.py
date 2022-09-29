42d3363e5a186b21e39bba2d69e6b791e4848ed9
sonam1331 <94792483+sonam1331@users.noreply.github.com>
2022-09-29 12:15:22 +0530

    Update main.py

--- prime/main.py
+++ prime/main.py
@@ -1,13 +1,15 @@
-num = 7
-if num >1:
-    for i in range(2,int(num/2)+1):
-        if (num%i)==0:
-            print(num, "not a prime no")
+import sys
+num1 = int(sys.argv[1])
+num1 = 7
+if num1 >1:
+    for i in range(2,int(num1/2)+1):
+        if (num1%i)==0:
+            print(num1, "not a prime no")
             break
         else:
-            print(num, "prime no")
+            print(num1, "prime no")
 else:
-    print(num,"not a prime no")
+    print(num1,"not a prime no")
 
 
 
